b2d1d65ce1700551c40738119776c05a723769a5
IBM_Admin <IBM_Admin@192.168.0.5>
2019-05-18 17:00:07 +0530

    push

--- 2019/flightreservation/src/main/java/com/sahil/flightreservation/repo/FlightRepository.java
+++ 2019/flightreservation/src/main/java/com/sahil/flightreservation/repo/FlightRepository.java
@@ -17,3 +17,4 @@ public interface FlightRepository extends JpaRepository<Flight, Long> {
 	List <Flight> findFlights(@Param("departureCity")String from,@Param("arrivalCity") String to,@Param("dateofDeparture") Date departureDate);
 
 }
+	
